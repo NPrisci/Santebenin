f4c4c0d194ec5d32a946550452188ee81a6f59d6
Maurel666-creator <maurelzinsou56@outlook.com>
2026-06-18 12:41:43 +0100

    style: update styling and label for login button on home page

--- src/pages/public/HomePage.vue
+++ src/pages/public/HomePage.vue
@@ -1,8 +1,8 @@
 <template>
    <div class="container-fluid align-items-center h-100">
       <h1>Bienvenue sur BeninSanté</h1>
-      <button class="btn btn-secondary" @click="test">Test toast</button>
-      <button class="btn btn-success" @click="login">Test login</button>
+      <button class="btn btn-secondary m-3" @click="test">Test toast</button>
+      <button class="btn btn-success" @click="login">login</button>
    </div>
 </template>
 
