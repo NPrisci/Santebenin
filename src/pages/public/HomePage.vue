f578957830d137b7d3380385ad8adc712874adac
Maurel666-creator <maurelzinsou56@outlook.com>
2026-06-18 12:41:21 +0100

    feat: add test buttons with toast notification and login navigation to HomePage

--- src/pages/public/HomePage.vue
+++ src/pages/public/HomePage.vue
@@ -1,5 +1,23 @@
 <template>
-   <div class="container-fluid d-flex align-items-center justify-content-center h-100">
+   <div class="container-fluid align-items-center h-100">
       <h1>Bienvenue sur BeninSanté</h1>
+      <button class="btn btn-secondary" @click="test">Test toast</button>
+      <button class="btn btn-success" @click="login">Test login</button>
    </div>
 </template>
+
+<script setup>
+import { useToast } from "@/composables/useToast";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const toast = useToast();
+
+const test = () => {
+   toast.success('Success message')
+}
+
+const login = () => {
+   router.push({ name: 'login' })
+}
+</script>
